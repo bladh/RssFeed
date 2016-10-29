9a8150b4b116376ee5f8052866115e00ab23a7d3
Alexander Bladh <alexander.bladh@tedsys.se>
2016-10-29 09:28:47 +0200

    Add base Android project
    Basic project structure including a base activity and fragment.

---
+++ app/src/main/java/se/tedsys/rssfeed/FeedFragment.java
@@ -0,0 +1,64 @@
+package se.tedsys.rssfeed;
+
+import android.app.Fragment;
+import android.content.Context;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class FeedFragment extends Fragment {
+
+    private FeedFragmentListener mListener;
+
+    /**
+     * Mandatory empty constructor for the fragment manager to instantiate the
+     * fragment (e.g. upon screen orientation changes).
+     */
+    public FeedFragment() {
+    }
+
+    public static FeedFragment newInstance(int columnCount) {
+        FeedFragment fragment = new FeedFragment();
+        Bundle args = new Bundle();
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        if (getArguments() != null) {
+            //set up fragment after saved parameters
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_feed, container, false);
+    }
+
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        if (context instanceof FeedFragmentListener) {
+            mListener = (FeedFragmentListener) context;
+        } else {
+            throw new RuntimeException(context.toString()
+                    + " must implement OnListFragmentInteractionListener");
+        }
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+        mListener = null;
+    }
+
+    public interface FeedFragmentListener {
+        //no methods yet
+    }
+}
