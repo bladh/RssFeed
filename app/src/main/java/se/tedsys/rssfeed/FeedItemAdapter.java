f91e75178e039a66d1e62893b98a166e44161cbf
Alexander Bladh <alexander.bladh@tedsys.se>
2016-10-29 13:10:13 +0200

    Display all attributes of the feed item in the view
    Previously, only the title was displayed.

--- app/src/main/java/se/tedsys/rssfeed/FeedItemAdapter.java
+++ app/src/main/java/se/tedsys/rssfeed/FeedItemAdapter.java
@@ -31,7 +31,10 @@ public class FeedItemAdapter extends RecyclerView.Adapter<FeedItemAdapter.ViewHo
 
     @Override
     public void onBindViewHolder(ViewHolder holder, int position) {
-        holder.mTitle.setText(mDataset.get(position).title);
+        final FeedItem item = mDataset.get(position);
+        holder.mTitle.setText(item.title);
+        holder.mAuthor.setText(item.author);
+        holder.mDate.setText(item.publishedDate);
 
     }
 
@@ -42,10 +45,15 @@ public class FeedItemAdapter extends RecyclerView.Adapter<FeedItemAdapter.ViewHo
 
     public static class ViewHolder extends RecyclerView.ViewHolder {
         // each data item is just a string in this case
-        public TextView mTitle;
+        TextView mTitle;
+        TextView mAuthor;
+        TextView mDate;
+
         public ViewHolder(View v) {
             super(v);
             mTitle = (TextView) itemView.findViewById(R.id.card_title);
+            mAuthor = (TextView) itemView.findViewById(R.id.card_author);
+            mDate = (TextView) itemView.findViewById(R.id.card_date);
         }
     }
 }
